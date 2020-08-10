84c59ce743df2b218ea162b196c85f310f5e97e6
Shashi Gowda <gowda@mit.edu>
2020-08-10 12:38:55 -0400

    docstrings for exec and compute

--- src/parallelism.jl
+++ src/parallelism.jl
@@ -4,6 +4,12 @@ lazy(f; kw...) = delayed(f; kw...)
 maybe_lazy(f, x) = any(x->x isa Union{Thunk, Chunk}, x) ? lazy(f)(x...) : f(x...)
 maybe_lazy(f) = (x...) -> maybe_lazy(f, x)
 
+"""
+    compute(tree::FileTree; cache=true)
+
+Compute any lazy values (Thunks) in `tree` and return a new tree where the values refer to
+the computed values (maybe on remote processes). The tree still behaves as a Lazy tree. `exec` on it will fetch the values from remote processes.
+"""
 compute(d::FileTree; cache=true, kw...) = compute(Dagger.Context(), d; cache=cache, kw...)
 function compute(ctx, d::FileTree; cache=true, kw...)
     thunks = []
@@ -27,4 +33,11 @@ end
 
 exec(d::FileTree) = mapvalues(exec, compute(d, cache=false); lazy=false)
 exec(d::Union{Thunk, Chunk}) = collect(compute(d))
+"""
+    exec(x)
+
+If `x` is a FileTree, computes any uncomputed `Thunk`s stored as values in it. Returns a new tree with the computed values.
+If `x` is a `Thunk` (such as the result of a `reducevalues`), then exec will compute the result.
+If `x` is anything else, `exec` just returns the same value.
+"""
 exec(x) = x
